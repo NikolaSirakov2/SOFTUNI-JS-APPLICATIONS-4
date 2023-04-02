0d91d2e89fbbe2e201f0431731f32091d9c1b81d
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-04-02 19:50:52 +0300

    Update app.js

--- 03. JS-Applications-Data-and-Authentication-Exercise-Resources/02.Phonebook/app.js
+++ 03. JS-Applications-Data-and-Authentication-Exercise-Resources/02.Phonebook/app.js
@@ -1,5 +1,48 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById("btnLoad").addEventListener("click", getInfo)
+}
+
+function showContacts(data){
+    let container = document.getElementById("phonebook");
+
+    console.log(data);
+
+    let content = Object.keys(data)
+
+    console.log(content);
+//     .map((entry) => `${entry.person}: ${entry.phone}`)
+//     .join("\n");
+
+//   container.textContent = content;
+
+}
+
+async function getInfo(){
+    const url = "http://localhost:3030/jsonstore/phonebook";
+    const response = await fetch(url);
+    const data = await response.json();
+
+    showContacts(data);
+}
+
+async function createNew(person, phone){
+    const url = "http://localhost:3030/jsonstore/phonebook";
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          person: person,
+          phone: phone
+        }),
+      });
+
+      const data = await response.json();
+
+    console.log(data);
 }
 
 attachEvents();
+
+getInfo()
