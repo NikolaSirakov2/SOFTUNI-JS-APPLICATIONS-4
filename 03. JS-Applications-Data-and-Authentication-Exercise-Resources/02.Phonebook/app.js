d7a0874fef473f7546b8d147fc2f7848cb469c12
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-04-03 09:40:30 +0300

    Update app.js

--- 03. JS-Applications-Data-and-Authentication-Exercise-Resources/02.Phonebook/app.js
+++ 03. JS-Applications-Data-and-Authentication-Exercise-Resources/02.Phonebook/app.js
@@ -1,28 +1,51 @@
 function attachEvents() {
-    document.getElementById("btnLoad").addEventListener("click", getInfo)
+    document.getElementById("btnLoad").addEventListener("click", getInfo);
+    document.getElementById("btnCreate").addEventListener("click", createContact)
 }
 
 function showContacts(data){
+    
     let container = document.getElementById("phonebook");
-
     console.log(data);
+    container.innerHTML = "";
 
-    let content = Object.keys(data)
+    let content = Object.values(data)
+    content.forEach(person => {
+        let pers = document.createElement("li");
+        let butt = document.createElement("button");
+        pers.innerText = `${person.person}: ${person.phone}`;
+        butt.innerText = "Delete";
+        
+       butt.addEventListener("click", deleteP(person._id));
 
-    console.log(content);
-//     .map((entry) => `${entry.person}: ${entry.phone}`)
-//     .join("\n");
-
-//   container.textContent = content;
-
+        pers.append(butt);
+        container.appendChild(pers);
+    })
 }
+
+function createContact(){
+    const person = document.getElementById("person");
+    const phone = document.getElementById("phone");
+
+    createNew(person.value, phone.value)
+    person.value = "";
+    phone.value = "";
+}
+
+function deleteP(id){
+    
+    const idNum = id;
+    deletePerson(idNum)
+    
+}
+
 
 async function getInfo(){
     const url = "http://localhost:3030/jsonstore/phonebook";
     const response = await fetch(url);
     const data = await response.json();
 
-    showContacts(data);
+    return showContacts(data);
 }
 
 async function createNew(person, phone){
@@ -40,9 +63,23 @@ async function createNew(person, phone){
 
       const data = await response.json();
 
-    console.log(data);
+    return data;
 }
 
-attachEvents();
+async function deletePerson(id){
+    const url = `http://localhost:3030/jsonstore/phonebook/${id}`;
 
-getInfo()
+    const response = await fetch(url, {
+        method: "DELETE",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(null),
+      });
+
+      const information = await response.json();
+
+    return information;
+}
+
+attachEvents()
